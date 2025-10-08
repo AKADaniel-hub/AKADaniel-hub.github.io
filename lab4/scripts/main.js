c6fb17b7855cc4832ce277b36b4247ade5f6ef1c
AKADaniel-hub <156716013+AKADaniel-hub@users.noreply.github.com>
2025-10-08 08:38:22 +0000

    simple javaScript Lab4

---
+++ lab4/scripts/main.js
@@ -0,0 +1,25 @@
+function hello() {
+        alert('HELLOOO!!')
+}
+
+
+function hide() {
+    const caixa = document.querySelector("#caixa");
+    caixa.textContent = "never touche me again!!!";
+}
+
+function dontLeave() {
+    const caixa = document.querySelector("#caixa");
+    caixa.textContent = "never leave me";
+}
+
+
+let counter = 0;
+function count() {
+    counter++;
+    alert(counter);
+}
+
+function handsome() {
+        alert('your soo awesome')
+}
